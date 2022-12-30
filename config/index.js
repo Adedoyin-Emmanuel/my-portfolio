7125566d23eee9651d31c6e37f42e4e308f71e64
Adedoyin Emmanuel Adeniyi <adedoyine535@gmail.com>
2022-12-30 05:22:11 +0100

    final updates

--- config/index.js
+++ config/index.js
@@ -2,8 +2,8 @@
 
 // EMAILJS Config Data
 
-export const EMAILJS_TEMPLATE_ID = ""
+export const EMAILJS_TEMPLATE_ID = "emmy-portfoilo";
 
-export const EMAILJS_SERVICE_ID = ""
+export const EMAILJS_SERVICE_ID = "service_mtg916s";
 
-export const EMAILJS_PUBLIC_KEY = ""
+export const EMAILJS_PUBLIC_KEY = "SJW9ktRhuAlOfvcXD";
